15f6ee6df89f25054de1616d5f3de3438c94594c
aghafoorhfd <ilghafoor@hotmail.com>
2024-08-16 19:57:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello, World!'
+
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
-
-
-@app.route('/health')
-def health_check():
-    return 'Health OK'
+    app.run(host='0.0.0.0', port=5000)
